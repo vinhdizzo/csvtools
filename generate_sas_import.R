d7fa06d23445baa9747337206bfc70d7c4d7305f
Vinh Nguyen <vinhdizzo@gmail.com>
2015-10-26 17:55:20 -0700

    &dlm macro variable specification; don't surround by quote because tab is '09'x

--- generate_sas_import.R
+++ generate_sas_import.R
@@ -43,7 +43,7 @@ input_statement <- paste('input\n', input_statement, '\n;\n', collapse='\n')
 if(!is.na(opt$output))
   sink(file=ofile, type='output')
 cat("data &dsn ;
-    infile &ifile DLM='|' DSD MISSOVER LRECL=")
+    infile &ifile DLM=&dlm DSD MISSOVER LRECL=")
 cat(sprintf('%d ', pmax(sum(d1$data_length)+10, 32767)))
 cat("FIRSTOBS=2 ;
 ")
